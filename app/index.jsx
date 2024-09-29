a97de2aa08fbaf1fd8be0fadb9c41de5d5979128
Mustafa <yusufmustufa@gmail.com>
2024-09-29 23:09:55 +0530

    Sttting up the project

---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import {Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl' >Mustafa</Text>
+      <StatusBar style="auto" />
+      <Link href={'/Profile'} className="text-blue-700" > Click to go to Profile</Link>
+    </View>
+  );
+};
+
+export default index;
+
