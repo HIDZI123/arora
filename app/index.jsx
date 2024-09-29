f63386eeef0d597f3ba90347d5fcfd33252dad01
Mustafa <yusufmustufa@gmail.com>
2024-09-29 23:16:35 +0530

    added images

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ import { StatusBar } from "expo-status-bar";
 const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl' >Mustafa</Text>
+      <Text className='text-3xl' >Mustafa in React-Native</Text>
       <StatusBar style="auto" />
       <Link href={'/Profile'} className="text-blue-700" > Click to go to Profile</Link>
     </View>
